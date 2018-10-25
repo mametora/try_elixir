064bbe66e79da6f65d2d72e9d8e1d4d8e3f320a7
mametora <mametora@mmtr.me>
2018-10-25 18:49:48 +0900

    mapsum追加

--- my.exs
+++ my.exs
@@ -1,4 +1,7 @@
 defmodule MyList do
   def sum([ head | [] ]),   do: head
   def sum([ head | tail ]), do: head + sum(tail)
+
+  def mapsum([ head | [] ], func), do: func.(head)
+  def mapsum([ head | tail ], func), do: func.(head) + mapsum(tail, func)
 end
