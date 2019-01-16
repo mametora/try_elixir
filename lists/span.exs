ec7547b2d18beaa5e4293c3eca9e81d0f5fe46e9
mametora <mametora@mmtr.me>
2019-01-17 00:55:12 +0900

    とりあえずtoまでのnの倍数のリストを返す関数を実装

--- lists/span.exs
+++ lists/span.exs
@@ -4,4 +4,12 @@ defmodule MyList do
   defp span(from, to, list) when from == to, do: list ++ [from]
 
   def span(from, to), do: span(from, to, [])
+
+  defp _multiple(n, to, i, list) when to < n * i, do: list
+
+  defp _multiple(n, to, i, list) do
+    _multiple(n, to, i + 1, list ++ [n * i])
+  end
+
+  def multiple(n, to), do: _multiple(n, to, 1, [])
 end
